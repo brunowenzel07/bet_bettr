a6eeb6092564a0b6a15b5ebd9c161e80c385c72e
elahmo <ahmetnovalic@gmail.com>
2015-05-22 10:43:42 +0200

    Added main application file, added classes and their initialization methods, according to modeling instructions

---
+++ app.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+from flask import Flask, request, flash, url_for, redirect, \
+     render_template, abort
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_pyfile('app.cfg')
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column('todo_id', db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    text = db.Column(db.String)
+    done = db.Column(db.Boolean)
+    pub_date = db.Column(db.DateTime)
+
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+        self.done = False
+        self.pub_date = datetime.utcnow()
+
+class User(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    Email = db.Column(db.String(120))
+    Name = db.Column(db.String(60))
+    DateSignedUp = db.Column(db.DateTime)
+
+    def __init__(self, Email, Name, DateSignedUp):
+        self.Email = Email
+        self.Name = Name
+        self.DateSignedUp = DateSignedUp
+
+class User_Countries(db.Model):
+    Userid = db.Column(db.Integer)
+    Countryid = db.Column(db.Integer)
+    DateStart = db.Column(db.DateTime)
+    Active = db.Column(db.Boolean)
+
+    def __init__(Userid, Countryid, DateStart, Active):
+        self.Userid = Userid
+        self.Countryid = Countryid
+        self.DateStart = DateStart
+        self.Active = Active
+
+class Racecourses(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(60), unique=True)
+
+    def __init__(self, Name):
+        self.Name = Name
+
+class Selections(db.Model): 
+    Userid = db.Column(db.Integer)
+    Racecourseid = db.Column(db.Integer)
+    RaceDate = db.Column(db.DateTime)
+    RaceNumber = db.Column(db.Integer)
+    HorseNumber = db.Column(db.Integer)
+    __table_args__ = (UniqueConstraint('Userid', 'Racecourseid', 'RaceDate', 'RaceNumber', 'HorseNumber'),
+                     )
+
+    def __init__(Userid, Racecourseid, RaceDate, RaceNumber, HorseNumber):
+        self.Userid = Userid
+        self.Racecourseid = Racecourseid
+        self.RaceDate = RaceDate
+        self.RaceNumber = RaceNumber
+        self.HorseNumber = HorseNumber
+
+@app.route('/')
+def indext():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
